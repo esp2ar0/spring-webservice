006f7d11a854963d62107843b2d7bd72d653964b
ChangHwan-Lee <dlckdghks66@naver.com>
2018-11-29 16:26:47 +0900

    3. SpringBoot & Handlebars로 화면 만들기

---
+++ src/main/java/com/esp2ar0/webservice/service/PostsService.java
@@ -0,0 +1,29 @@
+package com.esp2ar0.webservice.service;
+
+import com.esp2ar0.webservice.domain.posts.PostsRepository;
+import com.esp2ar0.webservice.dto.PostsMainResponseDto;
+import com.esp2ar0.webservice.dto.posts.PostsSaveRequestDto;
+import lombok.AllArgsConstructor;
+import org.springframework.stereotype.Service;
+import org.springframework.transaction.annotation.Transactional;
+
+import java.util.List;
+import java.util.stream.Collectors;
+
+@AllArgsConstructor
+@Service
+public class PostsService {
+    private PostsRepository postsRepository;
+
+    @Transactional
+    public Long save(PostsSaveRequestDto dto){
+        return postsRepository.save(dto.toEntity()).getId();
+    }
+
+    @Transactional(readOnly = true)
+    public List<PostsMainResponseDto> findAllDesc() {
+        return postsRepository.findAllDesc()
+                .map(PostsMainResponseDto::new)
+                .collect(Collectors.toList());
+    }
+}
